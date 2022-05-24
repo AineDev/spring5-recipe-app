eb32e5d3a6a3d86893ab462d7c6fa68c56457a06
Kearns <aine.kearns@accenture.com>
2022-05-24 17:10:34 +0200

    added sql statements to add content

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (category_name) VALUES ('French');
+INSERT INTO category (category_name) VALUES ('Japanese');
+INSERT INTO category (category_name) VALUES ('Italian');
+INSERT INTO category (category_name) VALUES ('Indian');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('tsp');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('tbsp');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('cup');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('pinch');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('grams');
