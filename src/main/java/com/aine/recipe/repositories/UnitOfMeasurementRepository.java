fba5d0cdbe1266e5b873e743ea45ae81f9145f8e
Kearns <aine.kearns@accenture.com>
2022-05-27 13:29:25 +0200

    added content into repo

--- src/main/java/com/aine/recipe/repositories/UnitOfMeasurementRepository.java
+++ src/main/java/com/aine/recipe/repositories/UnitOfMeasurementRepository.java
@@ -3,5 +3,9 @@ package com.aine.recipe.repositories;
 import com.aine.recipe.domain.UnitOfMeasurement;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasurementRepository extends CrudRepository<UnitOfMeasurement, Long> {
+
+    Optional<UnitOfMeasurement> findByUnitOfMeasurement(String unitOfMeasurement);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,6 @@ INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('tbsp');
 INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('cup');
 INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('pinch');
 INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('grams');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('whole');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('clove');
+INSERT INTO unit_of_measurement (unit_of_measurement) VALUES ('slices');
